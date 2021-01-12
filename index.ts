1541fbe431f0b175d2dbfb6c6523b2282fc2fb6c
tyautyau56 <i19yanagi@gmail.com>
2021-01-13 00:30:57 +0900

    change import

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 import { ServerRequest } from "https://deno.land/std@0.83.0/http/server.ts"
 import { soxa } from "https://deno.land/x/soxa/mod.ts"
 
-import { Model } from "./model/model"
-
+import { Model } from "./model/model.ts"
 
 
