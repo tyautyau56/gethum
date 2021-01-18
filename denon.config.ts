2f8404d9d0b980642ef377c4c661f2ea5094b4fb
tyautyau56 <i19yanagi@gmail.com>
2021-01-18 21:13:03 +0900

    add allow flag

--- denon.config.ts
+++ denon.config.ts
@@ -3,14 +3,16 @@ import { DenonConfig } from "https://deno.land/x/denon/mod.ts";
 const config: DenonConfig = {
     scripts: {
         dev: {
-            cmd: "deno index.ts",
+            cmd: "deno run --importmap=import_map.json index.ts",
             allow: [
                 "run",
-                "net"
+                "net",
+                "read",
+                "env"
             ],
             watch: true,
-            unstable: true
-        }
+            unstable: true,
+        },
     }
 };
 
